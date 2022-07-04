95f7c027d96941ef5f8f45a9d704165e9df3a413
Kaaleyah <41757315+Kaaleyah@users.noreply.github.com>
2022-07-04 18:37:27 +0300

    Some fixes

--- gui/main.js
+++ gui/main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 900,
+    width: 800,
+    height: 600,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
